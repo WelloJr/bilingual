3161fb006d48c8d2a9749fd8408a75bd0a8758f0
WelloJr <footballahly@gmail.com>
2024-12-10 22:28:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,36 @@
 import java.io.IOException;
+import java.util.HashMap;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
 
-public class TFIDFMapper extends Mapper<Object, Text, Text, Text> {
+public class TFIDFReducer extends Reducer<Text, Text, Text, Text> {
     private Text outputKey = new Text();
     private Text outputValue = new Text();
 
-    public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-        // Input format: Either <term@docID, TF> or <term, IDF>
-        String line = value.toString();
-        String[] parts = line.split("\\t");
+    public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+        // Input: <term, [TF@docID=TF, IDF=value]>
+        double idf = 0.0;
+        HashMap<String, Double> tfMap = new HashMap<>();
 
-        if (parts[0].contains("@")) {
-            // TF Input: <term@docID, TF>
-            String[] termDoc = parts[0].split("@");
-            String term = termDoc[0];
-            String docID = termDoc[1];
-            outputKey.set(term);
-            outputValue.set("TF@" + docID + "=" + parts[1]); // Emit <term, TF@docID=TF>
-        } else {
-            // IDF Input: <term, IDF>
-            String term = parts[0];
-            String idf = parts[1];
-            outputKey.set(term);
-            outputValue.set("IDF=" + idf); // Emit <term, IDF=value>
+        // Parse TF and IDF values
+        for (Text value : values) {
+            String val = value.toString();
+            if (val.startsWith("TF@")) {
+                String[] parts = val.split("=");
+                String docID = parts[0].substring(3); // Extract docID
+                double tf = Double.parseDouble(parts[1]); // Extract TF
+                tfMap.put(docID, tf);
+            } else if (val.startsWith("IDF=")) {
+                idf = Double.parseDouble(val.split("=")[1]); // Extract IDF
+            }
         }
-        context.write(outputKey, outputValue);
+
+        // Calculate TF-IDF and emit results
+        for (String docID : tfMap.keySet()) {
+            double tfidf = tfMap.get(docID) * idf;
+            outputKey.set(docID);
+            outputValue.set(key.toString() + ":" + tfidf); // <docID, term:TFIDF>
+            context.write(outputKey, outputValue);
+        }
     }
 }
