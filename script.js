0752e3b771a373dd06b2a1c390cb693d5e597432
WelloJr <footballahly@gmail.com>
2024-12-10 17:42:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,58 +1,73 @@
 package part2;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
 
-public class PartTwoDriver {
+public class PartTwoDriver extends Configuration implements Tool {
     public static void main(String[] args) throws Exception {
-        Configuration conf = new Configuration();
-        FileSystem fs = FileSystem.get(conf);
+        int exitCode = ToolRunner.run(new Configuration(), new PartTwoDriver(), args);
+        System.exit(exitCode);
+    }
 
-        // TF Job
-        Job tfJob = Job.getInstance(conf, "TF Calculation");
+    @Override
+    public int run(String[] args) throws Exception {
+        if (args.length != 3) {
+            System.err.println("Usage: PartTwoDriver <input path> <intermediate path> <output path>");
+            return -1;
+        }
+
+        Path inputPath = new Path(args[0]);
+        Path intermediatePathTF = new Path(args[1] + "_tf");
+        Path intermediatePathIDF = new Path(args[1] + "_idf");
+        Path outputPath = new Path(args[2]);
+
+        // Step 1: TF Job
+        Job tfJob = Job.getInstance(getConf(), "TF Calculation");
         tfJob.setJarByClass(PartTwoDriver.class);
         tfJob.setMapperClass(TFMapper.class);
         tfJob.setReducerClass(TFReducer.class);
         tfJob.setOutputKeyClass(Text.class);
         tfJob.setOutputValueClass(Text.class);
 
-        FileInputFormat.addInputPath(tfJob, new Path(args[0]));
-        Path tfOutput = new Path(args[1] + "_tf");
-        FileOutputFormat.setOutputPath(tfJob, tfOutput);
-        if (fs.exists(tfOutput)) fs.delete(tfOutput, true);
-        tfJob.waitForCompletion(true);
+        FileInputFormat.addInputPath(tfJob, inputPath);
+        FileOutputFormat.setOutputPath(tfJob, intermediatePathTF);
 
-        // IDF Job
-        Job idfJob = Job.getInstance(conf, "IDF Calculation");
+        if (!tfJob.waitForCompletion(true)) {
+            return 1;
+        }
+
+        // Step 2: IDF Job
+        Job idfJob = Job.getInstance(getConf(), "IDF Calculation");
         idfJob.setJarByClass(PartTwoDriver.class);
         idfJob.setMapperClass(IDFMapper.class);
         idfJob.setReducerClass(IDFReducer.class);
         idfJob.setOutputKeyClass(Text.class);
         idfJob.setOutputValueClass(Text.class);
 
-        FileInputFormat.addInputPath(idfJob, tfOutput);
-        Path idfOutput = new Path(args[1] + "_idf");
-        FileOutputFormat.setOutputPath(idfJob, idfOutput);
-        if (fs.exists(idfOutput)) fs.delete(idfOutput, true);
-        idfJob.waitForCompletion(true);
+        FileInputFormat.addInputPath(idfJob, intermediatePathTF);
+        FileOutputFormat.setOutputPath(idfJob, intermediatePathIDF);
 
-        // TFIDF Job
-        Job tfidfJob = Job.getInstance(conf, "TFIDF Calculation");
+        if (!idfJob.waitForCompletion(true)) {
+            return 1;
+        }
+
+        // Step 3: TF-IDF Job
+        Job tfidfJob = Job.getInstance(getConf(), "TF-IDF Calculation");
         tfidfJob.setJarByClass(PartTwoDriver.class);
         tfidfJob.setMapperClass(TFIDFMapper.class);
         tfidfJob.setReducerClass(TFIDFReducer.class);
         tfidfJob.setOutputKeyClass(Text.class);
         tfidfJob.setOutputValueClass(Text.class);
 
-        FileInputFormat.addInputPath(tfidfJob, idfOutput);
-        Path tfidfOutput = new Path(args[2]);
-        FileOutputFormat.setOutputPath(tfidfOutput);
-        if (fs.exists(tfidfOutput)) fs.delete(tfidfOutput, true);
-        tfidfJob.waitForCompletion(true);
+        FileInputFormat.addInputPath(tfidfJob, intermediatePathIDF);
+        FileOutputFormat.setOutputPath(tfidfJob, outputPath);
+
+        return tfidfJob.waitForCompletion(true) ? 0 : 1;
     }
 }
