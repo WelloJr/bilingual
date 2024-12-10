ccdccfb95a5545126bbb2923d6150c61821a33ac
WelloJr <footballahly@gmail.com>
2024-12-10 22:43:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.Mapper;
 
-public class TFReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+public class IDFMapper extends Mapper<Object, Text, Text, Text> {
 
-    private IntWritable result = new IntWritable();
+    private Text term = new Text();
+    private Text docID = new Text();
 
-    public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
-        int sum = 0;
-        for (IntWritable val : values) {
-            sum += val.get();
+    public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+        // Input: <term doc1: freq ; doc2: freq ; ...>
+        String line = value.toString();
+        String[] parts = line.split("\\t");
+
+        if (parts.length == 2) {
+            String termValue = parts[0];
+            String docDetails = parts[1];
+
+            String[] docList = docDetails.split(";");
+            for (String doc : docList) {
+                term.set(termValue);
+                docID.set(doc.split(":")[0].trim()); // Only emit the document ID
+                context.write(term, docID);
+            }
         }
-        result.set(sum);
-        context.write(key, result);
     }
 }
