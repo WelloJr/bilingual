f8348bdac7736ee0708394823314643ff78619e0
WelloJr <footballahly@gmail.com>
2024-12-10 18:03:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,20 @@
 package part2;
 
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.Mapper;
 
 import java.io.IOException;
 
-public class IDFReducer extends Reducer<Text, Text, Text, Text> {
-    private Text result = new Text();
+public class TFIDFMapper extends Mapper<Object, Text, Text, Text> {
+    private Text term = new Text();
+    private Text docAndTfIdf = new Text();
 
     @Override
-    protected void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-        int docCount = 0;
-        for (Text ignored : values) {
-            docCount++;
-        }
-
-        int totalDocs = 10; // Update this if the total number of documents changes
-        double idf = Math.log10((double) totalDocs / docCount);
-        result.set(docCount + " | " + idf);
-        context.write(key, result);
+    protected void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+        String line = value.toString().trim();
+        String[] parts = line.split("\\s+", 2);
+        term.set(parts[0]);
+        docAndTfIdf.set(parts[1]);
+        context.write(term, docAndTfIdf);
     }
 }
