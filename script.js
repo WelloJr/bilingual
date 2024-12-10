4906332725e13677fd0237363e9a5259389b8543
WelloJr <footballahly@gmail.com>
2024-12-10 23:08:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,43 @@
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.Path;
+package com.example.tfidf;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
 
-public class TFDriver {
-    public static void main(String[] args) throws Exception {
-        if (args.length != 2) {
-            System.err.println("Usage: TFDriver <input path> <output path>");
-            System.exit(-1);
+public class TF {
+    // TF Mapper
+    public static class TFMapper extends Mapper<Object, Text, Text, IntWritable> {
+        private final static IntWritable one = new IntWritable(1);
+        private Text wordDocPair = new Text();
+
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            String[] parts = value.toString().split("\\t", 2);
+            String docId = parts[0];
+            String content = parts[1];
+            StringTokenizer itr = new StringTokenizer(content);
+            while (itr.hasMoreTokens()) {
+                String word = itr.nextToken().toLowerCase();
+                wordDocPair.set(word + "@" + docId);
+                context.write(wordDocPair, one);
+            }
         }
+    }
 
-        // Create a Configuration object
-        Configuration conf = new Configuration();
+    // TF Reducer
+    public static class TFReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+        private IntWritable result = new IntWritable();
 
-        // Initialize the job
-        Job job = new Job(conf, "Term Frequency");
-
-        // Set the jar class
-        job.setJarByClass(TFDriver.class);
-
-        // Set the Mapper and Reducer classes
-        job.setMapperClass(TFMapper.class);
-        job.setReducerClass(TFReducer.class);
-
-        // Set the output key and value types
-        job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(IntWritable.class);
-
-        // Set the input and output paths
-        FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
-
-        // Exit after job completion
-        System.exit(job.waitForCompletion(true) ? 0 : 1);
+        public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+            int sum = 0;
+            for (IntWritable val : values) {
+                sum += val.get();
+            }
+            result.set(sum);
+            context.write(key, result);
+        }
     }
 }
