a2e946a468525d46331af1224cd28fc8cca24981
WelloJr <footballahly@gmail.com>
2024-12-10 22:43:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,19 @@
 import java.io.IOException;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
 
-public class IDFMapper extends Mapper<Object, Text, Text, Text> {
+public class IDFReducer extends Reducer<Text, Text, Text, Text> {
 
-    private Text term = new Text();
-    private Text docID = new Text();
+    private static final int TOTAL_DOCS = 10; // Update this based on the total number of documents
 
-    public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-        // Input: <term doc1: freq ; doc2: freq ; ...>
-        String line = value.toString();
-        String[] parts = line.split("\\t");
-
-        if (parts.length == 2) {
-            String termValue = parts[0];
-            String docDetails = parts[1];
-
-            String[] docList = docDetails.split(";");
-            for (String doc : docList) {
-                term.set(termValue);
-                docID.set(doc.split(":")[0].trim()); // Only emit the document ID
-                context.write(term, docID);
-            }
+    public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+        int docCount = 0;
+        for (Text value : values) {
+            docCount++;
         }
+
+        // Calculate IDF: log(TOTAL_DOCS / docCount)
+        double idf = Math.log((double) TOTAL_DOCS / docCount);
+        context.write(key, new Text(String.valueOf(idf)));
     }
 }
