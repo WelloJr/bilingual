83bc4c9e4505529bc202c73a909e426383a5ac1c
WelloJr <footballahly@gmail.com>
2024-12-10 23:39:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,72 +1,31 @@
-package com.example.tfidf;
+package tf;
 
-import java.io.IOException;
-
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Mapper;
-import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class TFIDF {
-    // TF-IDF Mapper
-    public static class TFIDFMapper extends Mapper<Object, Text, Text, Text> {
-        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-            // Split input line into parts
-            String[] parts = value.toString().split("\\t");
-            if (parts.length < 2) {
-                // Skip malformed input
-                return;
-            }
-
-            // Extract word@docId and TF value
-            String[] wordDoc = parts[0].split("@");
-            if (wordDoc.length < 2) {
-                // Skip malformed input
-                return;
-            }
-            String word = wordDoc[0];
-            String docId = wordDoc[1];
-            String tf = parts[1];
-
-            // Emit word as key, and docId=tf as value
-            context.write(new Text(word), new Text(docId + "=" + tf));
+    public static void main(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.err.println("Usage: TFIDF <input path> <output path>");
+            System.exit(-1);
         }
-    }
 
-    // TF-IDF Reducer
-    public static class TFIDFReducer extends Reducer<Text, Text, Text, DoubleWritable> {
-        private DoubleWritable tfidf = new DoubleWritable();
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "TF-IDF Calculation");
+        job.setJarByClass(TFIDF.class);
+        job.setMapperClass(TFIDFMapper.class);
+        job.setReducerClass(TFIDFReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(DoubleWritable.class);
 
-        public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-            int totalDocs = 10; // Example: total number of documents
-            int docCount = 0;
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
-            // Collect all values for this key
-            StringBuilder allValues = new StringBuilder();
-            for (Text val : values) {
-                allValues.append(val.toString()).append(",");
-                docCount++;
-            }
-
-            // Calculate IDF
-            double idf = Math.log10((double) totalDocs / (1 + docCount));
-
-            // Split back into individual TF values and calculate TF-IDF
-            String[] tfValues = allValues.toString().split(",");
-            for (String tfValue : tfValues) {
-                if (tfValue.isEmpty()) continue; // Skip empty values
-
-                String[] docTf = tfValue.split("=");
-                if (docTf.length < 2) continue; // Skip malformed input
-
-                String docId = docTf[0];
-                double tf = Double.parseDouble(docTf[1]);
-                double tfidfValue = tf * idf;
-
-                // Emit final TF-IDF value
-                tfidf.set(tfidfValue);
-                context.write(new Text(key.toString() + "@" + docId), tfidf);
-            }
-        }
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
