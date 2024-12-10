462f0392cb724b97d507114fde3b2ce1e124ff94
WelloJr <footballahly@gmail.com>
2024-12-10 22:25:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
-import java.io.IOException;
-import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
-public class IDFReducer extends Reducer<Text, IntWritable, Text, Text> {
-    private static final int TOTAL_DOCS = 10;
-
-    public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
-        int docCount = 0;
-        for (IntWritable val : values) {
-            docCount += val.get();
+public class IDFDriver {
+    public static void main(String[] args) throws Exception {
+        if (args.length < 3) {
+            System.err.println("Usage: IDFDriver <input path> <output path> <totalDocuments>");
+            System.exit(-1);
         }
 
-        double idf = Math.log((double) TOTAL_DOCS / docCount);
-        context.write(key, new Text(String.valueOf(idf)));
+        Configuration conf = new Configuration();
+        conf.set("totalDocuments", args[2]); // Pass total document count as a configuration parameter
+
+        Job job = new Job(conf, "IDF Calculation"); // Java 7-compatible Job instantiation
+        job.setJarByClass(IDFDriver.class);
+        job.setMapperClass(IDFMapper.class);
+        job.setReducerClass(IDFReducer.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
