29008d89a574dd44e80d4c9ace0c0b35bc9e31e2
WelloJr <footballahly@gmail.com>
2024-12-10 22:21:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
-import java.io.IOException;
-import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
-public class IDFMapper extends Mapper<Object, Text, Text, IntWritable> {
-    private final static IntWritable one = new IntWritable(1);
-    private Text word = new Text();
-
-    public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-        // Input: < term, doc1 freq1 doc2 freq2 ... >
-        String line = value.toString();
-        String[] parts = line.split("\\t");
-        if (parts.length > 1) {
-            String term = parts[0];
-            for (int i = 1; i < parts.length; i++) {
-                if (!parts[i].equals("0")) {
-                    word.set(term);
-                    context.write(word, one);
-                }
-            }
-        }
+public class TFDriver {
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        Job job = new Job(conf, "Term Frequency"); // Job instantiation for Java 7
+        job.setJarByClass(TFDriver.class);
+        job.setMapperClass(TFMapper.class);
+        job.setReducerClass(TFReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
