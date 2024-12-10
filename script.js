529a48bf0470dc4be851e7c5d582b8bf6321fa01
WelloJr <footballahly@gmail.com>
2024-12-10 20:51:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,25 @@
 package part2;
 
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
-import java.util.HashSet;
 
-public class IDFMapper extends Mapper<Object, Text, Text, Text> {
-    private Text term = new Text();
-    private Text docId = new Text();
+public class IDFReducer extends Reducer<Text, Text, Text, Text> {
+    private Text result = new Text();
 
     @Override
-    protected void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-        String line = value.toString().trim();
-        String[] parts = line.split("\\s+", 2);
-        String termText = parts[0];
-        String docData = parts[1];
+    protected void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+        int docCount = 0;
 
-        // Use a HashSet to avoid duplicate documents
-        HashSet<String> uniqueDocs = new HashSet<>();
-        String[] docs = docData.split(";");
-        for (String doc : docs) {
-            String docIdPart = doc.split(":")[0];
-            uniqueDocs.add(docIdPart);
+        for (Text value : values) {
+            docCount++;  // Count the number of documents for each term
         }
 
-        // Emit each term and document ID pair for further processing
-        for (String uniqueDoc : uniqueDocs) {
-            term.set(termText);
-            docId.set(uniqueDoc);
-            context.write(term, docId);
-        }
+        // Compute IDF: IDF = log10(N / DF), where N = 10 (total number of documents)
+        double idf = Math.log10(10.0 / docCount);
+
+        result.set(String.valueOf(idf));
+        context.write(key, result);
     }
 }
