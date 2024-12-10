e8f680fcd36f561d23c3d043e25a5d3115398eae
WelloJr <footballahly@gmail.com>
2024-12-10 22:44:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
 import java.io.IOException;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.Mapper;
 
-public class IDFReducer extends Reducer<Text, Text, Text, Text> {
+public class TFIDFMapper extends Mapper<Object, Text, Text, Text> {
 
-    private static final int TOTAL_DOCS = 10; // Update this based on the total number of documents
+    private Text outputKey = new Text();
+    private Text outputValue = new Text();
 
-    public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-        int docCount = 0;
-        for (Text value : values) {
-            docCount++;
+    public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+        // Input: <term@docID, TF> from TF
+        // Input: <term, IDF> from IDF
+        String line = value.toString();
+        String[] parts = line.split("\\t");
+
+        if (parts.length == 2) {
+            String term = parts[0];
+            String data = parts[1];
+
+            if (data.contains("=")) {
+                // TF Input: <term@docID, TF>
+                String[] termDoc = term.split("@");
+                outputKey.set(termDoc[0]);
+                outputValue.set("TF@" + termDoc[1] + "=" + data);
+            } else {
+                // IDF Input: <term, IDF>
+                outputKey.set(term);
+                outputValue.set("IDF=" + data);
+            }
+            context.write(outputKey, outputValue);
         }
-
-        // Calculate IDF: log(TOTAL_DOCS / docCount)
-        double idf = Math.log((double) TOTAL_DOCS / docCount);
-        context.write(key, new Text(String.valueOf(idf)));
     }
 }
