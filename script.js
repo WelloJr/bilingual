cfb1ddd0516805fcec914fa24cc82840323bba6e
WelloJr <footballahly@gmail.com>
2024-12-10 17:59:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,87 +1,34 @@
 package part2;
 
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.conf.Configurable;
-import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.apache.hadoop.util.Tool;
-import org.apache.hadoop.util.ToolRunner;
+import org.apache.hadoop.mapreduce.Mapper;
 
-public class PartTwoDriver implements Tool, Configurable {
+import java.io.IOException;
 
-    private Configuration conf;
-
-    public static void main(String[] args) throws Exception {
-        int exitCode = ToolRunner.run(new PartTwoDriver(), args);
-        System.exit(exitCode);
-    }
+public class TFMapper extends Mapper<Object, Text, Text, Text> {
+    private Text term = new Text();
+    private Text docAndCount = new Text();
 
     @Override
-    public void setConf(Configuration conf) {
-        this.conf = conf;
-    }
+    protected void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+        String line = value.toString().trim();
+        if (line.startsWith("<") && line.endsWith(">")) {
+            String content = line.substring(1, line.length() - 1).trim();
+            String[] parts = content.split("\\s+", 2);
+            String termText = parts[0];
+            String docData = parts[1];
 
-    @Override
-    public Configuration getConf() {
-        return this.conf;
-    }
-
-    @Override
-    public int run(String[] args) throws Exception {
-        if (args.length != 3) {
-            System.err.println("Usage: PartTwoDriver <input path> <intermediate path> <output path>");
-            return -1;
+            String[] docs = docData.split(";");
+            for (String doc : docs) {
+                String[] docParts = doc.trim().split(":");
+                if (docParts.length == 2) {
+                    String docId = docParts[0].trim();
+                    int count = docParts[1].split(",").length;
+                    term.set(termText);
+                    docAndCount.set(docId + ":" + count);
+                    context.write(term, docAndCount);
+                }
+            }
         }
-
-        Path inputPath = new Path(args[0]);
-        Path intermediatePathTF = new Path(args[1] + "_tf");
-        Path intermediatePathIDF = new Path(args[1] + "_idf");
-        Path outputPath = new Path(args[2]);
-
-        // Step 1: TF Job
-        Job tfJob = Job.getInstance(conf, "TF Calculation");
-        tfJob.setJarByClass(PartTwoDriver.class);
-        tfJob.setMapperClass(TFMapper.class);
-        tfJob.setReducerClass(TFReducer.class);
-        tfJob.setOutputKeyClass(Text.class);
-        tfJob.setOutputValueClass(Text.class);
-
-        FileInputFormat.addInputPath(tfJob, inputPath);
-        FileOutputFormat.setOutputPath(tfJob, intermediatePathTF);
-
-        if (!tfJob.waitForCompletion(true)) {
-            return 1;
-        }
-
-        // Step 2: IDF Job
-        Job idfJob = Job.getInstance(conf, "IDF Calculation");
-        idfJob.setJarByClass(PartTwoDriver.class);
-        idfJob.setMapperClass(IDFMapper.class);
-        idfJob.setReducerClass(IDFReducer.class);
-        idfJob.setOutputKeyClass(Text.class);
-        idfJob.setOutputValueClass(Text.class);
-
-        FileInputFormat.addInputPath(idfJob, intermediatePathTF);
-        FileOutputFormat.setOutputPath(idfJob, intermediatePathIDF);
-
-        if (!idfJob.waitForCompletion(true)) {
-            return 1;
-        }
-
-        // Step 3: TF-IDF Job
-        Job tfidfJob = Job.getInstance(conf, "TF-IDF Calculation");
-        tfidfJob.setJarByClass(PartTwoDriver.class);
-        tfidfJob.setMapperClass(TFIDFMapper.class);
-        tfidfJob.setReducerClass(TFIDFReducer.class);
-        tfidfJob.setOutputKeyClass(Text.class);
-        tfidfJob.setOutputValueClass(Text.class);
-
-        FileInputFormat.addInputPath(tfidfJob, intermediatePathIDF);
-        FileOutputFormat.setOutputPath(tfidfJob, outputPath);
-
-        return tfidfJob.waitForCompletion(true) ? 0 : 1;
     }
 }
