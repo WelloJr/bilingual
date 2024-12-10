9431d882c2dbaa6065c0e9107dc2e26a8c430aa9
WelloJr <footballahly@gmail.com>
2024-12-10 22:48:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,36 +5,34 @@ import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
-public class IDFDriver {
+public class TFIDFDriver {
     public static void main(String[] args) throws Exception {
-        if (args.length < 3) {
-            System.err.println("Usage: IDFDriver <input path> <output path> <totalDocuments>");
+        if (args.length != 3) {
+            System.err.println("Usage: TFIDFDriver <TF input path> <IDF input path> <output path>");
             System.exit(-1);
         }
 
         // Create a Configuration object
         Configuration conf = new Configuration();
 
-        // Set the total number of documents as a configuration parameter
-        conf.set("totalDocuments", args[2]);
-
         // Initialize the job
-        Job job = new Job(conf, "IDF Calculation");
+        Job job = new Job(conf, "TF-IDF Calculation");
 
         // Set the jar class
-        job.setJarByClass(IDFDriver.class);
+        job.setJarByClass(TFIDFDriver.class);
 
         // Set the Mapper and Reducer classes
-        job.setMapperClass(IDFMapper.class);
-        job.setReducerClass(IDFReducer.class);
+        job.setMapperClass(TFIDFMapper.class);
+        job.setReducerClass(TFIDFReducer.class);
 
         // Set the output key and value types
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(Text.class);
 
-        // Set the input and output paths
-        FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        // Set the input and output paths for TF and IDF jobs
+        FileInputFormat.addInputPath(job, new Path(args[0])); // TF input path
+        FileInputFormat.addInputPath(job, new Path(args[1])); // IDF input path
+        FileOutputFormat.setOutputPath(job, new Path(args[2])); // TF-IDF output path
 
         // Exit after job completion
         System.exit(job.waitForCompletion(true) ? 0 : 1);
