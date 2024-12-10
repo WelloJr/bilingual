5eab89dca14ccd4bb29ad7aca8cc9b239d9c73c5
WelloJr <footballahly@gmail.com>
2024-12-11 00:52:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,70 +1,34 @@
 package part2;
 
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.conf.Configured;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.apache.hadoop.util.Tool;
-import org.apache.hadoop.util.ToolRunner;
+import org.apache.hadoop.mapreduce.Mapper;
 
-public class PartTwoDriver extends Configured implements Tool {
+import java.io.IOException;
 
-    public static void main(String[] args) throws Exception {
-        int exitCode = ToolRunner.run(new Configuration(), new PartTwoDriver(), args);
-        System.exit(exitCode);
-    }
+public class TFMapper extends Mapper<Object, Text, Text, Text> {
+
+    private Text term = new Text();
+    private Text docAndCount = new Text();
 
     @Override
-    public int run(String[] args) throws Exception {
-        if (args.length != 3) {
-            System.err.println("Usage: PartTwoDriver <input path> <intermediate path> <output path>");
-            return -1;
+    protected void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+        String line = value.toString().trim();
+        if (line.startsWith("<") && line.endsWith(">")) {
+            String content = line.substring(1, line.length() - 1).trim();
+            String[] parts = content.split("\\s+", 2);
+            String termText = parts[0];
+            String docData = parts[1];
+
+            String[] docs = docData.split(";");
+            for (String doc : docs) {
+                String[] docParts = doc.split(":");
+                String docId = docParts[0].trim();
+                int count = docParts[1].split(",").length;
+
+                term.set(termText);
+                docAndCount.set(docId + ":" + count);
+                context.write(term, docAndCount);
+            }
         }
-
-        Configuration conf = getConf();
-        FileSystem fs = FileSystem.get(conf);
-
-        // TF Job
-        Job tfJob = Job.getInstance(conf, "TF Calculation");
-        tfJob.setJarByClass(PartTwoDriver.class);
-        tfJob.setMapperClass(TFMapper.class);
-        tfJob.setReducerClass(TFReducer.class);
-        tfJob.setOutputKeyClass(Text.class);
-        tfJob.setOutputValueClass(Text.class);
-        FileInputFormat.addInputPath(tfJob, new Path(args[0]));
-        Path tfOutputPath = new Path(args[1] + "_tf");
-        FileOutputFormat.setOutputPath(tfJob, tfOutputPath);
-        if (fs.exists(tfOutputPath)) fs.delete(tfOutputPath, true);
-        if (!tfJob.waitForCompletion(true)) return 1;
-
-        // IDF Job
-        Job idfJob = Job.getInstance(conf, "IDF Calculation");
-        idfJob.setJarByClass(PartTwoDriver.class);
-        idfJob.setMapperClass(IDFMapper.class);
-        idfJob.setReducerClass(IDFReducer.class);
-        idfJob.setOutputKeyClass(Text.class);
-        idfJob.setOutputValueClass(Text.class);
-        FileInputFormat.addInputPath(idfJob, tfOutputPath);
-        Path idfOutputPath = new Path(args[1] + "_idf");
-        FileOutputFormat.setOutputPath(idfJob, idfOutputPath);
-        if (fs.exists(idfOutputPath)) fs.delete(idfOutputPath, true);
-        if (!idfJob.waitForCompletion(true)) return 1;
-
-        // TF-IDF Job
-        Job tfidfJob = Job.getInstance(conf, "TF-IDF Calculation");
-        tfidfJob.setJarByClass(PartTwoDriver.class);
-        tfidfJob.setMapperClass(TFIDFMapper.class);
-        tfidfJob.setReducerClass(TFIDFReducer.class);
-        tfidfJob.setOutputKeyClass(Text.class);
-        tfidfJob.setOutputValueClass(Text.class);
-        FileInputFormat.addInputPath(tfidfJob, idfOutputPath);
-        Path tfidfOutputPath = new Path(args[2]);  // Ensure this is a Path object
-        FileOutputFormat.setOutputPath(tfidfJob, tfidfOutputPath);
-        if (fs.exists(tfidfOutputPath)) fs.delete(tfidfOutputPath, true);
-        return tfidfJob.waitForCompletion(true) ? 0 : 1;
     }
 }
