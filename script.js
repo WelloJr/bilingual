651eabafc5128ef9e28e0a23ecf44dd843379dbe
WelloJr <footballahly@gmail.com>
2024-12-17 22:57:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,104 @@
-https://www.kaggle.com/datasets/taranvee/smart-home-dataset-with-weather-information?resource=download
+Device,Energy
+Fridge,100
+TV,50
+Fridge,120
+Washing Machine,200
+Fridge,130
+Microwave,60
+Dishwasher,150
+TV,80
+Oven,300
+Fridge,110
+Fridge,105
+Washing Machine,220
+TV,70
+Microwave,55
+Dishwasher,145
+Oven,310
+Fridge,125
+Microwave,70
+TV,65
+Fridge,115
+Washing Machine,210
+Dishwasher,160
+TV,75
+Microwave,65
+Oven,320
+Fridge,140
+TV,60
+Dishwasher,155
+Washing Machine,230
+Microwave,80
+TV,85
+Fridge,130
+Fridge,95
+Oven,280
+Washing Machine,240
+TV,55
+Microwave,75
+Dishwasher,165
+Fridge,135
+Oven,290
+TV,90
+Microwave,60
+Dishwasher,150
+Washing Machine,250
+Fridge,120
+Microwave,50
+Dishwasher,170
+TV,100
+Fridge,110
+Washing Machine,260
+TV,95
+Microwave,85
+Dishwasher,140
+Oven,330
+Fridge,125
+TV,80
+Microwave,75
+Dishwasher,160
+Fridge,105
+Washing Machine,220
+Dishwasher,180
+Microwave,55
+TV,70
+Fridge,130
+Oven,340
+Washing Machine,210
+Microwave,60
+Dishwasher,175
+TV,75
+Fridge,140
+Washing Machine,230
+Oven,300
+Microwave,90
+Fridge,115
+TV,50
+Dishwasher,165
+Washing Machine,200
+Fridge,100
+Microwave,80
+Dishwasher,155
+TV,60
+Washing Machine,240
+Fridge,120
+Oven,280
+Microwave,65
+Dishwasher,150
+Fridge,125
+TV,90
+Washing Machine,230
+Microwave,75
+Dishwasher,145
+Fridge,110
+Oven,310
+TV,55
+Microwave,50
+Dishwasher,175
+Fridge,130
+TV,85
+Washing Machine,210
+Microwave,70
+Dishwasher,160
+Oven,290
+Fridge,115
